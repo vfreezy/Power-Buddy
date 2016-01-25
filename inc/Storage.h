913e47a00ca148d7fa6c8cff959c8135a2acca39
vfreezy <vonfriesentm@gmail.com>
2016-01-25 15:39:59 -0500

    Basic Structure for External Storage

--- inc/Storage.h
+++ inc/Storage.h
@@ -1,4 +1,4 @@
-/*							SecureDigitalStorage.h
+/*								Storage.h
 !*******************************************************************************
 !*
 !*
@@ -19,20 +19,18 @@
 
 /*                          CLASS DEFINITION
 ******************************************************************************/
-class SecureDigitalStorage : public Storage
+
+class Storage
 {
 	private:
-		uint8_t SS;
+		char filename[MAX_FILE_SIZE];
+		char buffer[MAX_FILE_BUF_SIZE];
+		File* pFile;
 		
-		boolean open();
-		boolean close();
-		
-	public:
-		SecureDigitalStorage();
-		uint8_t readln();
-		uint8_t writeln(uint8_t msg);
-		
-		uint8_t setFile(uint8_t fn);
-		uint8_t getData();
-	
+		virtual boolean open();
+		virtual boolean close();
+	public:	
+
+		virtual uint8_t readln();
+		virtual uint8_t writeln(char msg);
 };
--- src/SecureDigitalStorage.cpp
+++ src/SecureDigitalStorage.cpp
@@ -13,3 +13,45 @@
 
 /*                              INCLUDES
 ******************************************************************************/
+#include "SecureDigitalStorage.h"
+#include "Common.h"
+#include "SD.h"
+
+
+SecureDigitalStorage::SecureDigitalStorage()
+{
+
+}
+
+boolean SecureDigitalStorage::open()
+{
+	if (this->filename != NULL)
+	{
+		
+	}else
+		return false;	
+}
+boolean SecureDigitalStorage::close()
+{
+	return true;
+}
+
+uint8_t SecureDigitalStorage::readln()
+{
+	
+}
+
+uint8_t SecureDigitalStorage::writeln()
+{
+	
+}
+
+uint8_t SecureDigitalStorage::setFile(uint8_t fn)
+{
+	
+}
+
+uint8_t SecureDigitalStorage::getData()
+{
+
+}
